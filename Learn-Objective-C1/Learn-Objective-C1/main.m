e95ba5f37e485e891455513dd972e42ac3b63423
Anirvan <anirvan.mandal@gmail.com>
2012-06-22 22:25:02 +0530

    Adding files

---
+++ Learn-Objective-C1/Learn-Objective-C1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Learn-Objective-C1
+//
+//  Created by Anirvan Mandal on 22/06/12.
+//  Copyright (c) 2012 anirvan.mandal@gmail.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main()
+{      
+    printf("Hello World");
+    return 0;
+}
+
