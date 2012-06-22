b66b9d2f2a73a99bdba95cdf9ea313bb159ae395
Anirvan <anirvan.mandal@gmail.com>
2012-06-23 01:21:01 +0530

    Completed part 1

--- Learn-Objective-C1/Learn-Objective-C1/main.m
+++ Learn-Objective-C1/Learn-Objective-C1/main.m
@@ -10,7 +10,30 @@
 
 int main()
 {      
-    printf("Hello World");
+    printf("Hello World"); //Hello World
+    
+    int i = 0; //Defining variables
+    
+    if(i>1)//Condition
+    {
+        printf("This can never happen %i", i);
+    }
+    else
+    {
+        printf("Phew Aleast I know C");    
+    }
+    
+    while(i < 4) //LOOP
+    {
+        printf("This is a loop and i is %i", i);
+        i++;
+    }
+    
+    int *ptr = &i;
+    
+    printf("The value of ptr = %i, value = %i ",ptr,*ptr);//pointer
+    
+    
     return 0;
 }
 
